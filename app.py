3aa3f878d69cde99cc9afe1fd2801107e06477ff
SQY <770426840@qq.com>
2025-03-03 20:27:10 +0800

    1.2.2 第一个稳定版

--- app.py
+++ app.py
@@ -161,3 +161,4 @@ def download_file(filename):
 
 if __name__ == '__main__':
     app.run(debug=False, port=5000,use_reloader=False)
+    # app.run(host='0.0.0.0',debug=False, port=5000,use_reloader=False,ssl_context=('cert.pem', 'key.pem'))
